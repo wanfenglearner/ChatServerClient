0f36b671562bf893433e23b6f236d1b23625b022
wanfeng123 <2200726897@qq.com>
2024-12-04 22:02:01 +0800

    测试

---
+++ Server/friendmodel.h
@@ -0,0 +1,25 @@
+
+/*
+	操作数据库中的 friend 表
+*/
+#ifndef _FRIEND_MODEL_H_
+#define _FRIEND_MODEL_H_
+
+#include "user.h"
+#include <vector>
+
+class FriendModel
+{
+public:
+	// 添加好友
+	void insert(int userid, int friendid);
+	// 删除好友
+	void remove(int userid, int friendid);
+
+	// 获得好友的信息
+	std::vector<User> query(int id);
+
+
+};
+
+#endif
