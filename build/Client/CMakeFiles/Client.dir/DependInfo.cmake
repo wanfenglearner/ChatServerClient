fcc417d617154ca80a74972c33dad2ae3b1ffb74
robin <2200726897@qq.com>
2025-01-05 17:02:28 +0800

    Cmake构建

---
+++ build/Client/CMakeFiles/Client.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/robin/projects/Chat/Client/src/chatclient.cpp" "Client/CMakeFiles/Client.dir/src/chatclient.cpp.o" "gcc" "Client/CMakeFiles/Client.dir/src/chatclient.cpp.o.d"
+  "/home/robin/projects/Chat/Client/src/main.cpp" "Client/CMakeFiles/Client.dir/src/main.cpp.o" "gcc" "Client/CMakeFiles/Client.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/Server/CMakeFiles/Server.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/robin/projects/Chat/Server/src/ChatServer.cpp" "Server/CMakeFiles/Server.dir/src/ChatServer.cpp.o" "gcc" "Server/CMakeFiles/Server.dir/src/ChatServer.cpp.o.d"
+  "/home/robin/projects/Chat/Server/src/ChatService.cpp" "Server/CMakeFiles/Server.dir/src/ChatService.cpp.o" "gcc" "Server/CMakeFiles/Server.dir/src/ChatService.cpp.o.d"
+  "/home/robin/projects/Chat/Server/src/database.cpp" "Server/CMakeFiles/Server.dir/src/database.cpp.o" "gcc" "Server/CMakeFiles/Server.dir/src/database.cpp.o.d"
+  "/home/robin/projects/Chat/Server/src/friendmodel.cpp" "Server/CMakeFiles/Server.dir/src/friendmodel.cpp.o" "gcc" "Server/CMakeFiles/Server.dir/src/friendmodel.cpp.o.d"
+  "/home/robin/projects/Chat/Server/src/groupmodel.cpp" "Server/CMakeFiles/Server.dir/src/groupmodel.cpp.o" "gcc" "Server/CMakeFiles/Server.dir/src/groupmodel.cpp.o.d"
+  "/home/robin/projects/Chat/Server/src/main.cpp" "Server/CMakeFiles/Server.dir/src/main.cpp.o" "gcc" "Server/CMakeFiles/Server.dir/src/main.cpp.o.d"
+  "/home/robin/projects/Chat/Server/src/offlinemessagemodel.cpp" "Server/CMakeFiles/Server.dir/src/offlinemessagemodel.cpp.o" "gcc" "Server/CMakeFiles/Server.dir/src/offlinemessagemodel.cpp.o.d"
+  "/home/robin/projects/Chat/Server/src/redis.cpp" "Server/CMakeFiles/Server.dir/src/redis.cpp.o" "gcc" "Server/CMakeFiles/Server.dir/src/redis.cpp.o.d"
+  "/home/robin/projects/Chat/Server/src/usermodel.cpp" "Server/CMakeFiles/Server.dir/src/usermodel.cpp.o" "gcc" "Server/CMakeFiles/Server.dir/src/usermodel.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
